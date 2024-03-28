773af16729092df6e48e71be1f6d529d807eb213
Jeremy Fowler <jembyfowler@gmail.com>
2024-03-28 10:00:43 -0600

    field report class created

---
+++ app/models/FieldReport.js
@@ -0,0 +1,14 @@
+import { generateId } from "../utils/GenerateId.js"
+
+export class FieldReport {
+  constructor(data) {
+    // FIXME this might break later
+    this.id = generateId()
+    this.title = data.title
+    this.threatLevel = data.threatLevel
+    this.body = data.body
+    this.author = data.author
+    this.entryDate = new Date()
+    this.securityLevel = data.securityLevel
+  }
+}
