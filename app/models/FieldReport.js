c03d5523022d5536d5510205d672289af46fde43
Jeremy Fowler <jembyfowler@gmail.com>
2024-03-28 11:51:23 -0600

    added details template

--- app/models/FieldReport.js
+++ app/models/FieldReport.js
@@ -9,6 +9,7 @@ export class FieldReport {
     this.body = data.body || ''
     this.author = data.author
     this.createdAt = new Date()
+    this.lastViewed = new Date()
     this.securityLevel = data.securityLevel
   }
 
@@ -19,6 +20,21 @@ export class FieldReport {
         <p>${this.title}</p>
         <p>${this.securityLevel}</p>
         <p>${this.CreatedDate}</p>
+      </div>
+    </div>
+    `
+  }
+
+  get ActiveDetailsTemplate() {
+    return `
+    <div class="col-8">
+      <h1>${this.title}</h1>
+      <h2>Reported by Mick on 3/28/2024 10:35:01 AM</h2>
+      <h2>Last accessed 3/28/2024 11:00:00 AM</h2>
+      <h2 style="color: #ff1818;">Midnight</h2>
+      <div>
+        <label for="reportBody">Report Body</label>
+        <textarea name="body" id="reportBody"></textarea>
       </div>
     </div>
     `
